31ae56af0a233c3e1d3436a7f77c87c239e726e1
FubarTechA <tjfubar11@gmail.com>
2022-05-31 15:28:06 +0100

    new commit

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const allcountry = async function () {
   });
 };
 
-allcountry();
+// allcountry();
 
 const region = async function (reg) {
   const res = await fetch(`https://restcountries.com/v3.1/region/${reg}`);
@@ -40,13 +40,69 @@ const region = async function (reg) {
 const country = async function (name) {
   const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
   const data = await res.json();
-  console.log(data[0]);
-  countryName.textContent = data[0].name.official;
-  const flag = data[0].flags.svg;
-  countryFlag.setAttribute("src", flag);
-  countryPopulation.textContent = data[0].population;
-  countryRegion.textContent = data[0].region;
-  countryCapital.textContent = data[0].capital[0];
+  const country = data[0];
+  console.log(country);
+
+  const markup = `
+<div class="detailimg-div">
+<img src="${country.flags.svg}" alt="" class="detail-img" />
+</div>
+<div class="detail-info">
+<div class="info-div">
+  <div class="infoDiv1">
+    <h2 class="detail-name">${country.name.official}</h2>
+    <p>Native Name:<span class="detail-natName"></span></p>
+    <p>Population:<span class="detail-population">${country.population}</span></p>
+    <p>Region:<span class="detail-region">${country.region}</span></p>
+    <p>Sub Region<span class="detail-subRegion"></span></p>
+    <p>Capital:<span class="detail-capital">${country.capital}</span></p>
+  </div>
+  <div class="infoDiv2">
+    <p>Top Level Domain<span class="detail-domain"></span></p>
+    <p>Currencies<span class="detail-currency"></span></p>
+    <p>Languages<span class="detail-languages"></span></p>
+  </div>
+</div>
+<div class="detailborder-div">
+  <p>Border countries</p>
+  <div class="borders">
+    <button class="border-country">France</button>
+    <button class="border-country">Germany</button>
+    <button class="border-country">Netherlands</button>
+  </div>
+</div>
+</div>
+`;
 };
 
-// country("nigeria");
+country("nigeria");
+// const markup = `
+// <div class="detailimg-div">
+// <img src="" alt="" class="detail-img" />
+// </div>
+// <div class="detail-info">
+// <div class="info-div">
+//   <div class="infoDiv1">
+//     <h2 class="detail-name">Belgium</h2>
+//     <p>Native Name:<span class="detail-natName"></span></p>
+//     <p>Population:<span class="detail-population"></span></p>
+//     <p>Region:<span class="detail-region"></span></p>
+//     <p>Sub Region<span class="detail-subRegion"></span></p>
+//     <p>Capital:<span class="detail-capital"></span></p>
+//   </div>
+//   <div class="infoDiv2">
+//     <p>Top Level Domain<span class="detail-domain"></span></p>
+//     <p>Currencies<span class="detail-currency"></span></p>
+//     <p>Languages<span class="detail-languages"></span></p>
+//   </div>
+// </div>
+// <div class="detailborder-div">
+//   <p>Border countries</p>
+//   <div class="borders">
+//     <button class="border-country">France</button>
+//     <button class="border-country">Germany</button>
+//     <button class="border-country">Netherlands</button>
+//   </div>
+// </div>
+// </div>
+// `;
