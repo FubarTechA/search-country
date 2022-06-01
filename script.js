7aa0a4b5503498d5ce957afc7a568e01b680ba3b
FubarTechA <tjfubar11@gmail.com>
2022-06-02 00:39:14 +0100

    new commit

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
 const countryFlag = document.querySelector(".country-flag");
 
 const wrapper = document.querySelector(".wrapper");
+const home = document.querySelector(".home");
+const detail = document.querySelector(".detail");
+
 const header = document.querySelector("header");
 const input = document.querySelector("input");
 const region = document.querySelector(".region");
+const regionInfo = document.querySelector(".region-info");
+const regionUl = document.querySelector(".region ul");
 const dropdown = document.querySelector(".dropdown");
 const country = document.querySelectorAll(".country");
 const dropdownIcon = document.querySelector(".dropdown-icon");
-console.log(country);
+const backBtn = document.querySelector(".backBtn");
+// console.log(regionUl);
 
 const countryName = document.querySelector(".country-name");
 const countryPopulation = document.querySelector(".country-population");
@@ -19,7 +25,6 @@ const detailMain = document.querySelector(".detail-main");
 const modeIcon = document.querySelector(".mode-icon");
 const modeAside = document.querySelector(".mode-aside");
 const modeSpan = document.querySelector(".mode-span");
-console.log(modeAside);
 
 const selectMode = function () {
   modeAside.addEventListener("click", function () {
@@ -48,7 +53,7 @@ const selectMode = function () {
   });
 };
 
-region.addEventListener("click", function () {
+regionInfo.addEventListener("click", function () {
   console.log("clicked");
   if (dropdown.classList.contains("show")) {
     dropdown.classList.remove("show");
@@ -61,23 +66,78 @@ region.addEventListener("click", function () {
   }
 });
 
-if (country) {
-  selectMode();
-}
+selectMode();
 
-const allcountry = async function () {
-  const res = await fetch("https://restcountries.com/v3.1/all");
+const regionFunc = async function (reg) {
+  const res = await fetch(`https://restcountries.com/v3.1/region/${reg}`);
   const data = await res.json();
-  console.log(data.map((country) => country.name.official));
   data.forEach((country) => {
     const markup = ` 
       <div class="country">
       <img src="${country.flags.png}" alt="" class="country-flag" />
               <div class="country-info">
                 <h2 class="country-name">${country.name.official}</h2>
-                <p>Population: <span class="country-population">${country.population}</span></p>
-                <p>Region: <span class="country-region">${country.region}</span></p>
-                <p>Capital: <span class="country-capital">${country.capital}</span></p>
+                <p>Population: <span class="country-population">${
+                  country.population
+                }</span></p>
+                <p>Region: <span class="country-region">${
+                  country.region
+                }</span></p>
+                <p>Capital: <span class="country-capital">${
+                  country.capital ? country.capital : "Not Available"
+                }</span></p>
+              </div>
+            </div>
+      `;
+    countryDiv.insertAdjacentHTML("beforeend", markup);
+  });
+};
+
+regionUl.addEventListener("click", function (e) {
+  const listitem = e.target.closest("li");
+  if (!listitem) return;
+  const region = listitem.textContent;
+
+  countryDiv.innerHTML = "";
+  regionFunc(region);
+  dropdown.classList.remove("show");
+  dropdownIcon.classList.remove("fa-angle-up");
+  dropdownIcon.classList.add("fa-angle-down");
+});
+
+regionUl.addEventListener("mouseover", function (e) {
+  const listitem = e.target.closest("li");
+  if (!listitem) return;
+  console.log(listitem);
+  listitem.classList.add("hover");
+});
+
+regionUl.addEventListener("mouseout", function (e) {
+  const listitem = e.target.closest("li");
+  if (!listitem) return;
+  console.log(listitem);
+  listitem.classList.remove("hover");
+});
+
+const allcountry = async function () {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  // console.log(data.map((country) => country.name.official));
+  data.forEach((country) => {
+    const markup = ` 
+      <div class="country">
+      <img src="${country.flags.png}" alt="" class="country-flag" />
+              <div class="country-info">
+                <h2 class="country-name">${country.name.official}</h2>
+                <p>Population: <span class="country-population">${
+                  country.population
+                }</span></p>
+                <p>Region: <span class="country-region">${
+                  country.region
+                }</span></p>
+                <p>Capital: <span class="country-capital">${
+                  country.capital ? country.capital : "Not Available"
+                }</span></p>
               </div>
             </div>
       `;
@@ -87,25 +147,36 @@ const allcountry = async function () {
 
 allcountry();
 
-const regionFunc = async function (reg) {
-  const res = await fetch(`https://restcountries.com/v3.1/region/${reg}`);
-  const data = await res.json();
-  console.log(data.map((country) => country.name.official));
-};
+countryDiv.addEventListener("click", function (e) {
+  const country = e.target.closest(".country");
+  if (!country) return;
+  // console.log(country);
+  const countryName = country.querySelector("h2").textContent;
+  console.log(countryName);
+  home.classList.add("hide");
+  detail.classList.remove("hide");
+  countryFunc(countryName);
+});
 
-// region("asia");
+detailMain.addEventListener("click", function (e) {
+  const borderCountry = e.target.closest(".border-country").textContent;
+  console.log(borderCountry);
+  detailMain.innerHTML = "";
+  countryFunc(borderCountry);
+});
+
+backBtn.addEventListener("click", function () {
+  detailMain.innerHTML = "";
+  home.classList.remove("hide");
+  detail.classList.add("hide");
+});
 
 const countryFunc = async function (name) {
   const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
   const data = await res.json();
   const country = data[0];
   console.log(country);
-  console.log(Object.values(country.currencies)[0].name);
-  console.log(
-    country.borders
-      .map((border) => `<button class="border-country">${border}</button>`)
-      .join("")
-  );
+  // console.log(Object.values(country.currencies)[0].name);
   const markup = `
 <div class="detailimg-div">
 <img src="${country.flags.svg}" alt="" class="detail-img" />
@@ -124,14 +195,18 @@ const countryFunc = async function (name) {
     }</span></p>
     <p>Region: <span class="detail-region">${country.region}</span></p>
     <p>Sub Region: <span class="detail-subRegion">${
-      country.subregion
+      country.subregion ? country.subregion : "Not Available"
     }</span></p>
-    <p>Capital: <span class="detail-capital">${country.capital}</span></p>
+    <p>Capital: <span class="detail-capital">${
+      country.capital ? country.capital : "Not Available"
+    }</span></p>
   </div>
   <div class="infoDiv2">
     <p>Top Level Domain: <span class="detail-domain">${country.tld}</span></p>
     <p>Currencies: <span class="detail-currency">${
-      Object.values(country.currencies)[0].name
+      country.currencies
+        ? Object.values(country.currencies)[0].name
+        : "No Official Currency"
     }</span></p>
     <p>Languages: <span class="detail-languages">${Object.values(
       country.languages
@@ -141,9 +216,13 @@ const countryFunc = async function (name) {
 <div class="detailborder-div">
   <p>Border countries</p>
   <div class="borders">
-  ${country.borders
-    .map((border) => `<button class="border-country">${border}</button>`)
-    .join("")}
+  ${
+    country.borders
+      ? country.borders
+          .map((border) => `<button class="border-country">${border}</button>`)
+          .join("")
+      : "None"
+  }
   </div>
 </div>
 </div>
@@ -151,4 +230,4 @@ const countryFunc = async function (name) {
   detailMain.insertAdjacentHTML("beforeend", markup);
 };
 
-// country("china");
+// countryFunc("BEN");
