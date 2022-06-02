9908822a2314655ff478e2864fb19dd91b6461fa
FubarTechA <tjfubar11@gmail.com>
2022-06-02 02:04:05 +0100

    new commit

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 "use strict";
-const countryFlag = document.querySelector(".country-flag");
 
+// selecting elements
+const countryFlag = document.querySelector(".country-flag");
 const wrapper = document.querySelector(".wrapper");
 const home = document.querySelector(".home");
 const detail = document.querySelector(".detail");
-
 const header = document.querySelector("header");
 const input = document.querySelector("input");
 const region = document.querySelector(".region");
 const regionInfo = document.querySelector(".region-info");
 const regionUl = document.querySelector(".region ul");
 const dropdown = document.querySelector(".dropdown");
+const dropdownLi = document.querySelectorAll(".dropdown li");
 const country = document.querySelectorAll(".country");
 const dropdownIcon = document.querySelector(".dropdown-icon");
 const backBtn = document.querySelector(".backBtn");
-// console.log(regionUl);
-
 const countryName = document.querySelector(".country-name");
 const countryPopulation = document.querySelector(".country-population");
 const countryRegion = document.querySelector(".country-region");
@@ -25,158 +24,45 @@ const detailMain = document.querySelector(".detail-main");
 const modeIcon = document.querySelector(".mode-icon");
 const modeAside = document.querySelector(".mode-aside");
 const modeSpan = document.querySelector(".mode-span");
+const borderCountry = document.querySelector(".border-country");
 
-const selectMode = function () {
-  modeAside.addEventListener("click", function () {
-    const country = document.querySelectorAll(".country");
+// functions
 
-    if (modeIcon.dataset.mode === "sun") {
-      modeIcon.classList.remove("fa-moon");
-      modeIcon.classList.add("fa-sun");
-      modeSpan.textContent = "Light";
-      modeIcon.dataset.mode = "moon";
-    } else {
-      modeIcon.classList.remove("fa-sun");
-      modeIcon.classList.add("fa-moon");
-      modeSpan.textContent = "Dark";
-      modeIcon.dataset.mode = "sun";
-    }
-    wrapper.classList.toggle("light");
-    header.classList.toggle("light");
-    input.classList.toggle("light");
-    region.classList.toggle("light");
-    dropdown.classList.toggle("light");
-    console.log(country);
-
-    country.forEach((country) => country.classList.toggle("light"));
-    console.log(input.getAttribute("placeholder"));
-  });
-};
-
-regionInfo.addEventListener("click", function () {
-  console.log("clicked");
-  if (dropdown.classList.contains("show")) {
-    dropdown.classList.remove("show");
-    dropdownIcon.classList.remove("fa-angle-up");
-    dropdownIcon.classList.add("fa-angle-down");
-  } else {
-    dropdown.classList.add("show");
-    dropdownIcon.classList.add("fa-angle-up");
-    dropdownIcon.classList.remove("fa-angle-down");
-  }
-});
-
-selectMode();
-
-const regionFunc = async function (reg) {
-  const res = await fetch(`https://restcountries.com/v3.1/region/${reg}`);
-  const data = await res.json();
-  data.forEach((country) => {
-    const markup = ` 
-      <div class="country">
-      <img src="${country.flags.png}" alt="" class="country-flag" />
-              <div class="country-info">
-                <h2 class="country-name">${country.name.official}</h2>
-                <p>Population: <span class="country-population">${
-                  country.population
-                }</span></p>
-                <p>Region: <span class="country-region">${
-                  country.region
-                }</span></p>
-                <p>Capital: <span class="country-capital">${
-                  country.capital ? country.capital : "Not Available"
-                }</span></p>
-              </div>
-            </div>
-      `;
-    countryDiv.insertAdjacentHTML("beforeend", markup);
-  });
-};
-
-regionUl.addEventListener("click", function (e) {
-  const listitem = e.target.closest("li");
-  if (!listitem) return;
-  const region = listitem.textContent;
-
-  countryDiv.innerHTML = "";
-  regionFunc(region);
+// to hide dropdown
+const hideDropdown = function () {
   dropdown.classList.remove("show");
   dropdownIcon.classList.remove("fa-angle-up");
   dropdownIcon.classList.add("fa-angle-down");
-});
-
-regionUl.addEventListener("mouseover", function (e) {
-  const listitem = e.target.closest("li");
-  if (!listitem) return;
-  console.log(listitem);
-  listitem.classList.add("hover");
-});
-
-regionUl.addEventListener("mouseout", function (e) {
-  const listitem = e.target.closest("li");
-  if (!listitem) return;
-  console.log(listitem);
-  listitem.classList.remove("hover");
-});
-
-const allcountry = async function () {
-  const res = await fetch("https://restcountries.com/v3.1/all");
-  const data = await res.json();
-  // console.log(data.map((country) => country.name.official));
-  data.forEach((country) => {
-    const markup = ` 
-      <div class="country">
-      <img src="${country.flags.png}" alt="" class="country-flag" />
-              <div class="country-info">
-                <h2 class="country-name">${country.name.official}</h2>
-                <p>Population: <span class="country-population">${
-                  country.population
-                }</span></p>
-                <p>Region: <span class="country-region">${
-                  country.region
-                }</span></p>
-                <p>Capital: <span class="country-capital">${
-                  country.capital ? country.capital : "Not Available"
-                }</span></p>
-              </div>
-            </div>
-      `;
-    countryDiv.insertAdjacentHTML("beforeend", markup);
-  });
 };
 
-allcountry();
+// to show dropdown
+const showDropDown = function () {
+  dropdown.classList.add("show");
+  dropdownIcon.classList.add("fa-angle-up");
+  dropdownIcon.classList.remove("fa-angle-down");
+};
 
-countryDiv.addEventListener("click", function (e) {
-  const country = e.target.closest(".country");
-  if (!country) return;
-  // console.log(country);
-  const countryName = country.querySelector("h2").textContent;
-  console.log(countryName);
-  home.classList.add("hide");
-  detail.classList.remove("hide");
-  countryFunc(countryName);
-});
+// function to displayCountries on home page
+const displayCountries = function (info) {
+  const markup = ` 
+  <div class="country">
+  <img src="${info.flags.png}" alt="" class="country-flag" />
+          <div class="country-info">
+            <h2 class="country-name">${info.name.official}</h2>
+            <p>Population: <span class="country-population">${
+              info.population
+            }</span></p>
+            <p>Region: <span class="country-region">${info.region}</span></p>
+            <p>Capital: <span class="country-capital">${
+              info.capital ? info.capital : "Not Available"
+            }</span></p>
+          </div>
+        </div>
+  `;
+  countryDiv.insertAdjacentHTML("beforeend", markup);
+};
 
-detailMain.addEventListener("click", function (e) {
-  const borderCountry = e.target.closest(".border-country").textContent;
-  console.log(borderCountry);
-  detailMain.innerHTML = "";
-  countryFunc(borderCountry);
-});
-
-backBtn.addEventListener("click", function () {
-  detailMain.innerHTML = "";
-  home.classList.remove("hide");
-  detail.classList.add("hide");
-});
-
-const countryFunc = async function (name) {
-  const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
-  const data = await res.json();
-  const country = data[0];
-  console.log(country);
-  // console.log(Object.values(country.currencies)[0].name);
+const displayParticularCountry = function (country) {
   const markup = `
 <div class="detailimg-div">
 <img src="${country.flags.svg}" alt="" class="detail-img" />
@@ -229,5 +115,164 @@ const countryFunc = async function (name) {
 `;
   detailMain.insertAdjacentHTML("beforeend", markup);
 };
+
+// function for displaying all countries on load
+const allcountry = async function () {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  data.forEach((country) => {
+    displayCountries(country);
+  });
+};
+
+// function that gets countries according to region
+const regionFunc = async function (reg) {
+  const res = await fetch(`https://restcountries.com/v3.1/region/${reg}`);
+  const data = await res.json();
+  data.forEach((country) => {
+    displayCountries(country);
+  });
+};
+
+// displaying particular country
+
+const countryFunc = async function (name) {
+  const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+  const data = await res.json();
+  const country = data[0];
+  // console.log(country);
+  displayParticularCountry(country);
+};
+
+// function for displaying the detail page
+const displayDetail = function () {
+  home.classList.add("hide");
+  detail.classList.remove("hide");
+};
+
+// function for toggling between light and dark mode
+const selectMode = function () {
+  modeAside.addEventListener("click", function () {
+    const country = document.querySelectorAll(".country");
+    const borderCountry = document.querySelectorAll(".border-country");
+
+    // adding all the elements that the light class will be toggled on
+    const arr = [wrapper, header, input, region, detail, backBtn];
+
+    // adding the dropdownli and country nodelist plus the arr array into a new arr
+    const secondArr = [arr, dropdownLi, country, borderCountry];
+
+    if (modeIcon.dataset.mode === "sun") {
+      modeIcon.classList.remove("fa-moon");
+      modeIcon.classList.add("fa-sun");
+      modeSpan.textContent = "Light";
+      modeIcon.dataset.mode = "moon";
+    } else {
+      modeIcon.classList.remove("fa-sun");
+      modeIcon.classList.add("fa-moon");
+      modeSpan.textContent = "Dark";
+      modeIcon.dataset.mode = "sun";
+    }
+
+    // looping through the secondArr and using the forEach method on each individual array
+    secondArr.forEach((arr) =>
+      arr.forEach((element) => element.classList.toggle("light"))
+    );
+  });
+};
+
+allcountry();
+selectMode();
+
+regionInfo.addEventListener("click", function () {
+  console.log("clicked");
+  if (dropdown.classList.contains("show")) {
+    hideDropdown();
+  } else {
+    showDropDown();
+  }
+});
+
+regionUl.addEventListener("click", function (e) {
+  const listitem = e.target.closest("li");
+  if (!listitem) return;
+
+  // selecting the region
+  const region = listitem.textContent;
+
+  // deleting the html from the countrydiv
+  countryDiv.innerHTML = "";
+
+  // displaying countries based on region selected
+  regionFunc(region);
+
+  // hiding the dropdown
+  hideDropdown();
+});
+
+// adding and removing the hover effect on the dropdown li
+regionUl.addEventListener("mouseover", function (e) {
+  const listitem = e.target.closest("li");
+  if (!listitem) return;
+  console.log(listitem);
+  listitem.classList.add("hover");
+});
+
+regionUl.addEventListener("mouseout", function (e) {
+  const listitem = e.target.closest("li");
+  if (!listitem) return;
+  console.log(listitem);
+  listitem.classList.remove("hover");
+});
+
+// removing the placeholder on focus
+input.addEventListener("focus", function () {
+  input.removeAttribute("placeholder");
+});
+
+document.addEventListener("keydown", function (e) {
+  // e.preventDefault();
+  if (e.key === "Enter") {
+    e.preventDefault();
+    const country = input.value;
+    input.value = "";
+    input.setAttribute("placeholder", "Search for a country...");
+    // display the detail page
+    displayDetail();
+    countryFunc(country);
+  }
+});
+
+countryDiv.addEventListener("click", function (e) {
+  const country = e.target.closest(".country");
+  if (!country) return;
+
+  // selecting the country
+  const countryName = country.querySelector("h2").textContent;
+
+  displayDetail();
+
+  // displaying the country info
+  countryFunc(countryName);
+});
+
+detailMain.addEventListener("click", function (e) {
+  // selecting border country
+  const borderCountry = e.target.closest(".border-country").textContent;
+
+  // deleting all html in the detail main section
+  detailMain.innerHTML = "";
+
+  // dislplaying border country
+  countryFunc(borderCountry);
+});
+
+backBtn.addEventListener("click", function () {
+  detailMain.innerHTML = "";
+
+  // showing the home page
+  home.classList.remove("hide");
+  detail.classList.add("hide");
+});
 
 // countryFunc("BEN");
